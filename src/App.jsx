9a1ac7109a118e16b11a64803133d3a106daa059
SRB77 <soumyach144@gmail.com>
2025-05-12 14:17:04 +0530

    Add complete incomplete feature Delete lists unique id with Date some styling fixes

--- src/App.jsx
+++ src/App.jsx
@@ -6,43 +6,32 @@ const App = () => {
   //* React variable means State to store current Value and List of todos
   const [content, setContent] = useState("");
   const [taskList, setTaskList] = useState([]);
-  const [check,setCheck] = useState(false);
-  
-  useEffect(()=>{
+
+  useEffect(() => {
     const storedTasks = JSON.parse(localStorage.getItem("taskList")) || [];
     setTaskList(storedTasks);
-  },[])
+  }, []);
 
-  
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.setItem("taskList", JSON.stringify(taskList));
-  },[taskList])
-
-  //* handling CheckBox functions . 
-  const handleCheck = ()=>{
-    if(check){
-      setCheck(false)
-    }else{
-      setCheck(true)
-    } 
-  }
+  }, [taskList]);
 
   //* Function for handling the Click of ADD btn .
   const handleClick = () => {
     if (content.trim() === "") return;
     else {
-      let newTask = {content:content,id:Date.now(),isCompleted:check};
+      let newTask = { content: content, id: Date.now(), isCompleted: false };
       setTaskList([...taskList, newTask]);
       setContent("");
     }
   };
 
-//* Function for handling the keyPress of ADD btn .
+  //* Function for handling the keyPress of ADD btn .
   const keyHandle = (event) => {
     if (event.key == "Enter") {
       if (content.trim() === "") return;
       else {
-        let newTask = { content: content, id: Date.now(), isCompleted: check };
+        let newTask = { content: content, id: Date.now(), isCompleted: false };
         setTaskList([...taskList, newTask]);
         setContent("");
       }
@@ -50,10 +39,23 @@ const App = () => {
   };
 
   //* Function for handling the Delete button .
-  const delButton = (indexDel) => {
-    const updateAftDel = taskList.filter((_, index) => index !== indexDel);
+  const delButton = (todoId) => {
+    const updateAftDel = taskList.filter((todos,index) => todos.id !== todoId);
     setTaskList(updateAftDel);
   };
+
+  //* Handle toglle of complete / incomplete
+  const handleToggle = (todoId)=>{
+    const updatedList = taskList.map((todos)=>{
+      if(todoId === todos.id){
+        return { ...todos, isCompleted: !todos.isCompleted};
+      }
+      return todos;
+    })
+    setTaskList(updatedList);
+  }
+
+
 
   return (
     <div className="main">
@@ -74,13 +76,20 @@ const App = () => {
           </button>
         </div>
         <div className="tasks_area">
-          {taskList.map((todoList, index) => {
+          {taskList.map((todoList) => {
             return (
-              <li key={index}>
-                {todoList.content}
+              <li key={todoList.id}>
+                <span className={todoList.isCompleted ? "completed" : ""}>
+                  {todoList.content}
+                </span>
                 <div className="extra-on-list">
-                  <input type="checkbox" className="checkBox" onChange={handleCheck} />
-                  <button onClick={() => delButton(index)}>Delete</button>
+                  <input
+                    type="checkbox"
+                    className="checkBox"
+                    onChange={() => handleToggle(todoList.id)}
+                    checked={todoList.isCompleted}
+                  />
+                  <button onClick={() => delButton(todoList.id)}>Delete</button>
                 </div>
               </li>
             );
